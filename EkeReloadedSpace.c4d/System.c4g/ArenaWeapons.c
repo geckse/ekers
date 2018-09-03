3c806a7443eaed886a7bc4cd5a6e2d79b3815116
arthurmoeller <16727664+arthurmoeller@users.noreply.github.com>
2018-09-03 21:13:55 +0200

    arena weapons script cleanup and rocket launcher fix

--- EkeReloadedSpace.c4d/System.c4g/ArenaWeapons.c
+++ EkeReloadedSpace.c4d/System.c4g/ArenaWeapons.c
@@ -3,24 +3,31 @@
 #strict 2
 #appendto SF5B
 
-public func RejectCollect(id idObject, object pObject)
-{
- var other = FindObject2(Find_ID(idObject),Find_Container(this));
- if(other && FindObject(ARWE) && LocalN("ammo",pObject) && !idObject == RL5B){
-   LocalN("ammo",other) = LocalN("ammo",other)+LocalN("ammo",pObject);
-   if(LocalN("ammo",other) >= 100) LocalN("ammo",other) = 100;
+public func RejectCollect(id idObject, object pObject) {
+    var other = FindObject2(Find_ID(idObject), Find_Container(this));
+    if (other && FindObject(ARWE)) {
+		
+		// move ammo in other object
+		if (LocalN("ammo", pObject)) {
+			LocalN("ammo", other) = LocalN("ammo", other) + LocalN("ammo", pObject);
+			if (LocalN("ammo", other) >= 100) LocalN("ammo", other) = 100;
 
-   if(idObject == AR5B && LocalN("gGrenades",pObject)){
-      LocalN("gGrenades",other) = LocalN("gGrenades",other)+LocalN("gGrenades",pObject);
-      if(LocalN("gGrenades",other) >= 100) LocalN("gGrenades",other) = 100;
-   }
-   pObject->~CastParticles("PxSpark",RandomX(4,12),RandomX(12,20),0,0,RandomX(30,60),60,RGBa(255,255,255),RGBa(225,225,255));
-   RemoveObject(pObject);
- }
-	/* 2do: rocket launcher reload
-	if(other && FindObject(ARWE) && idObject == RL5B && FindContents(MS5B, pObject) && !FindContents(MS5B, other)){
-		CreateContents(MS5B,other);
-		LocalN("ammo",other) = 100;
-	} */
- return _inherited(idObject,pObject);
+			if (idObject == AR5B && LocalN("gGrenades", pObject)) {
+				LocalN("gGrenades", other) = LocalN("gGrenades", other) + LocalN("gGrenades", pObject);
+				if (LocalN("gGrenades", other) >= 100) LocalN("gGrenades", other) = 100;
+			}
+		}
+        //  move missile in other rocket launcher
+        if (idObject == RL5B) {
+            if (FindContents(MS5B, pObject) && (ContentsCount(MS5B, other) == 0)) {
+                CreateContents(MS5B, other);
+                LocalN("ammo", other) = 100;
+            }
+        }
+		
+		// remove duplicate and cast particles
+        pObject->~CastParticles("PxSpark", RandomX(4, 12), RandomX(12, 20), 0, 0, RandomX(30, 60), 60, RGBa(255, 255, 255), RGBa(225, 225, 255));
+        RemoveObject(pObject);
+    }
+    return _inherited(idObject, pObject);
 }
