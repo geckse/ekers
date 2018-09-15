c56071ce79c1bff024110aa29a254b66ded8a72b
arthurmoeller <16727664+arthurmoeller@users.noreply.github.com>
2018-09-16 01:01:06 +0200

    new scen prototype

---
+++ EkeReloadedSpace.c4f/EkeRS_FogIsland.c4s/Script.c
@@ -0,0 +1,38 @@
+ /*-- Asteriod Meele --*/
+
+#strict
+
+func Initialize() {
+  // Sternenhimmel
+  SetSkyParallax(0, 100,70, 1,0);
+  CreateObject(COFA);  
+  return(ScriptGo(1));
+}
+func Script1()
+{
+  SetMaxPlayer();
+  return(1);
+}
+
+func Script2(){
+    for(var spwn in FindObjects(Find_ID(SPNP))) {
+    var Contents = [CA5B, OB5B, HG5B, GB5B, NH5B, GS5B, SG5B];
+    CreateContents(Contents[Random(8)], spwn);
+   }
+}
+func Script120(){
+     goto(2);
+}
+
+private func InitializeClonk(clonk)
+{ 
+    var wipf = PlaceAnimal(WIPF);
+    SetPosition(GetX(wipf), GetY(wipf)-10, clonk);
+    RemoveObject(wipf);
+
+    var assaultRifle = CreateContents(AR5B, clonk);
+    LocalN("ammo", assaultRifle) = 100;
+    LocalN("qGrenades", assaultRifle) = 100;
+    CreateContents(HG5B, clonk,1);
+    return(1);
+}
