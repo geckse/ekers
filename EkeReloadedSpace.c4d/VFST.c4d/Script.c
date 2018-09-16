bb12e615a6dc1189b466cbb06b2967c127f2b455
arthurmoeller <16727664+arthurmoeller@users.noreply.github.com>
2018-09-16 23:18:03 +0200

    deactivated crosshair

--- EkeReloadedSpace.c4d/VFST.c4d/Script.c
+++ EkeReloadedSpace.c4d/VFST.c4d/Script.c
@@ -15,8 +15,8 @@ func Initialize(){
   iO2 = MaxO2();
   AddEffect("Life", this(), 1, 35, this());
   iShootingAxis = 1;
-  CreateCrosshair();
-  AddEffect("Crosshair", this(), 1, 1, this());
+  // CreateCrosshair();
+  // AddEffect("Crosshair", this(), 1, 1, this());
   return(_inherited());
 }
 
@@ -195,7 +195,7 @@ func Entrance() {
     return(_inherited());
 }
 func Departure() {
-    CreateCrosshair();
+    //CreateCrosshair();
     return(_inherited());
 }
 func CrewSelection(deselect)
