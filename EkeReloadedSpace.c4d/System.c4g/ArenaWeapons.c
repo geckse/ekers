c120ee15593eb8b5757e69a94c232f0ced33fdc0
arthurmoeller <16727664+arthurmoeller@users.noreply.github.com>
2018-09-17 22:37:41 +0200

    fixed arena weapons rule

--- EkeReloadedSpace.c4d/System.c4g/ArenaWeapons.c
+++ EkeReloadedSpace.c4d/System.c4g/ArenaWeapons.c
@@ -1,4 +1,4 @@
-/*-- Neues Script --*/
+/*-- Arena Weapons --*/
 
 #strict 2
 #appendto SF5B
@@ -7,28 +7,28 @@ public func RejectCollect(id idObject, object pObject) {
     var other = FindObject2(Find_ID(idObject), Find_Container(this));
     if (other && FindObject(ARWE)) {
 		
-		// move ammo in other object
 		if (LocalN("ammo", pObject)) {
+          
+		    // move ammo in other weapon     
 			LocalN("ammo", other) = LocalN("ammo", other) + LocalN("ammo", pObject);
 			if (LocalN("ammo", other) >= 100) LocalN("ammo", other) = 100;
-
 			if (idObject == AR5B && LocalN("gGrenades", pObject)) {
 				LocalN("gGrenades", other) = LocalN("gGrenades", other) + LocalN("gGrenades", pObject);
 				if (LocalN("gGrenades", other) >= 100) LocalN("gGrenades", other) = 100;
 			}
-		}
-		
-        //  move missile in other rocket launcher
-        if (idObject == RL5B) {
-            if (FindContents(MS5B, pObject) && (ContentsCount(MS5B, other) == 0)) {
-                CreateContents(MS5B, other);
-                LocalN("ammo", other) = 100;
+          
+            //  move missile in other rocket launcher
+            if (idObject == RL5B) {
+                if (FindContents(MS5B, pObject) && (ContentsCount(MS5B, other) == 0)) {
+                    CreateContents(MS5B, other);
+                    LocalN("ammo", other) = 100;
+                }
             }
-        }
-		
-        // remove duplicate and cast particles
-        pObject->~CastParticles("PxSpark", RandomX(4, 12), RandomX(12, 20), 0, 0, RandomX(30, 60), 60, RGBa(255, 255, 255), RGBa(225, 225, 255));
-        RemoveObject(pObject);
+          
+            // remove duplicate and cast particles
+            pObject->~CastParticles("PxSpark", RandomX(4, 12), RandomX(12, 20), 0, 0, RandomX(30, 60), 60, RGBa(255, 255, 255), RGBa(225, 225, 255));
+            RemoveObject(pObject);
+		}
     }
     return _inherited(idObject, pObject);
 }
