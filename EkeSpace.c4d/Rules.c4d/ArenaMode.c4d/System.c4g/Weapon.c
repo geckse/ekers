8ee5870d730c0c15078b073835abe654e66dae45
arthurmoeller <16727664+arthurmoeller@users.noreply.github.com>
2018-10-05 20:14:30 +0200

    optimized rule script arena mode ammo regen

--- EkeSpace.c4d/Rules.c4d/ArenaMode.c4d/System.c4g/Weapon.c
+++ EkeSpace.c4d/Rules.c4d/ArenaMode.c4d/System.c4g/Weapon.c
@@ -15,13 +15,11 @@ func FxAmmoRegenTimer()
 }
 func ActivateWeapon()
 {
-  if(!arenaModeRule) return _inherited(...);
-  RemoveEffect("AmmoRegen",this);
+  if(arenaModeRule) RemoveEffect("AmmoRegen",this);
   return _inherited(...);
 }
 func DeactivateWeapon()
 {
-  if(!arenaModeRule) return _inherited(...);
-  AddEffect("AmmoRegen", this, 1, 2, this);
+  if(arenaModeRule) AddEffect("AmmoRegen", this, 1, 2, this);
   return _inherited(...);
 }
