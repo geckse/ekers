cfa846f4c2d4d8432f29c3786a476937c2468213
Markus Mittendrein <git@maxmitti.tk>
2018-10-12 01:13:14 +0200

    Add Select all and Invert selection to the CrewSelection rule

--- EkeSpace.c4d/Rules.c4d/CrewSelection.c4d/Script.c
+++ EkeSpace.c4d/Rules.c4d/CrewSelection.c4d/Script.c
@@ -17,6 +17,16 @@ func Activate(int player)
 		Menu_Style_Context(),
 
 		Menu_Factory([ObjectCallback("CrewFactory", true)], [player, selectionMark]),
+		Menu_Entry([
+			Menu_Entry_Caption("$SelectAll$"),
+			Menu_Entry_Callbacks([Menu_Callback(ObjectCallback("SelectAll", true), Menu_CallbackType_Defaults, [Menu_CallbackArg_Args, Menu_CallbackArg_Action, Menu_CallbackArg_MenuObject])]),
+			Menu_Entry_Args([player, 0, -3])
+		]),
+		Menu_Entry([
+			Menu_Entry_Caption("$InvertSelection$"),
+			Menu_Entry_Callbacks([Menu_Callback(ObjectCallback("InvertSelection", true), Menu_CallbackType_Defaults, [Menu_CallbackArg_Args, Menu_CallbackArg_Action, Menu_CallbackArg_MenuObject])]),
+			Menu_Entry_Args([player, 0, -2])
+		]),
 		Menu_Accept()
 	]);
 
@@ -65,25 +75,69 @@ func CrewFactory(array args, int entryNumber)
 	return entries;
 }
 
+func CheckReopenMenu(array args, object currentCursor)
+{
+	if(GetCursor(args[0]) != currentCursor)
+	{
+		Activate(args[0])->SelectEntry(args[2]);
+		return Menu_React_Close;
+	}
+}
+
+func CheckReopenOrCloseMenu(array args, int action, object currentCursor)
+{
+	if(action == Menu_CallbackType_Special2)
+	{
+		return CheckReopenMenu(args, currentCursor) || Menu_React_Refresh;
+	}
+	else
+	{
+		return Menu_React_Close;
+	}
+}
+
 func CrewSelection(array args, int action, object currentCursor)
 {
 	if(action == Menu_CallbackType_Special2)
 	{
 		SelectCrew(args[0], args[1], !IsCrewSelected(args[0], args[1]));
-
-		if(GetCursor(args[0]) != currentCursor)
-		{
-			Activate(args[0])->SelectEntry(args[2]);
-			return Menu_React_Close;
-		}
-		return Menu_React_Refresh;
 	}
 	else
 	{
 		UnselectCrew(args[0]);
 		SetCursor(args[0], args[1]);
-		return Menu_React_Close;
 	}
+	return CheckReopenOrCloseMenu(args, action, currentCursor);
+}
+
+func SelectAll(array args, int action, object currentCursor)
+{
+	for(var i = 0; i < GetCrewCount(args[0]); ++i)
+	{
+		var crew = GetCrew(args[0], i);
+		if(crew)
+		{
+			SelectCrew(args[0], crew, true);
+		}
+	}
+	return CheckReopenOrCloseMenu(args, action, currentCursor);
+}
+
+func InvertSelection(array args, int action, object currentCursor)
+{
+	for(var i = 0; i < GetCrewCount(args[0]); ++i)
+	{
+		var crew = GetCrew(args[0], i);
+		if(crew && crew != currentCursor)
+		{
+			SelectCrew(args[0], crew, !IsCrewSelected(args[0], crew));
+		}
+	}
+
+	// in case the current cursor is the first clonk it can't be unselected otherwise
+	SelectCrew(args[0], currentCursor, false);
+
+	return CheckReopenOrCloseMenu(args, action, currentCursor);
 }
 
 func HighlightCrew(array args)
