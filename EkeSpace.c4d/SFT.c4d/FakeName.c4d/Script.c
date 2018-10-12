f9bc252476c11359c2afed127867e65f88d226a7
arthurmoeller <16727664+arthurmoeller@users.noreply.github.com>
2018-10-12 02:22:21 +0200

    faked promotion message for clonks

--- EkeSpace.c4d/SFT.c4d/FakeName.c4d/Script.c
+++ EkeSpace.c4d/SFT.c4d/FakeName.c4d/Script.c
@@ -1,11 +1,14 @@
 #strict 2
 
 local clonkName;
+local clonkRank;
 
 func Recruitment()
 {
   clonkName = Format("%s", GetName());
   SetName("");
+  clonkRank = GetRank(this);
+  AddEffect("RankCheck", this, 1, 1, this);
   return _inherited(...);
 }
 func CrewSelection(deselect)
@@ -30,4 +33,20 @@ global func GetName(object target, id id)
     }
 
     return LocalN("clonkName", target || this) || inherited(target, ...);
+}
+
+func FxRankCheckTimer()
+{
+  var currentRank = GetRank(this);
+  if (currentRank != clonkRank)
+  {
+    var owner = GetOwner(this);
+    if(owner != NO_OWNER)
+    {
+      clonkRank = currentRank;
+      if(GetCursor(owner) == this) CustomMessage(Format("@%s|%s", $ClonkRanks$[clonkRank], clonkName), 0, owner, -5,55, 0, 0, "1", 1);
+      var RankUpMsg = Format("$RankUp$", clonkName, $ClonkRanks$[clonkRank]);
+      PlayerMessage(owner, RankUpMsg, this);
+    }
+  }
 }
