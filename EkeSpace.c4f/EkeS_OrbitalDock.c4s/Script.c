fbd11ad9f2e645320a9dc4d6da3d1b1028820c29
arthurmoeller <16727664+arthurmoeller@users.noreply.github.com>
2018-12-05 02:17:05 +0100

    Orbital Dock - spawnpoints fix once again, halfed chance for neohexin

--- EkeSpace.c4f/EkeS_OrbitalDock.c4s/Script.c
+++ EkeSpace.c4f/EkeS_OrbitalDock.c4s/Script.c
@@ -4,21 +4,23 @@
 
 static itemSpawns;
 static itemSpawnsL;
+static oldR;
 
 func Initialize() {
-  // Sternenhimmel
-  SetSkyParallax(0, 100,70, 1,0);
+    
+    // Sternenhimmel
+    SetSkyParallax(0, 100,70, 1,0);
 
-  // declare spawning items
-  itemSpawns = [CA7A, OB7A, GB7A, NH7A, SG7A, MS7A, HG7A, GS7A, RL7A, RB7A];
-  itemSpawnsL = GetLength(itemSpawns);
+    // declare spawning items
+    itemSpawns = [CA7A, OB7A, GB7A, NH7A, SG7A, GS7A, RL7A, RB7A];
+    itemSpawnsL = GetLength(itemSpawns);
 
-  return(ScriptGo(1));
+    return(ScriptGo(1));
 }
 func Script1()
 {
-  SetMaxPlayer();
-  return(1);
+    SetMaxPlayer();
+    return(1);
 }
 
 func Script2(){
@@ -26,10 +28,17 @@ func Script2(){
     // create items in spawn points
     for(var spwn in FindObjects(Find_ID(IS7A))) {
 
-        var r = Random(itemSpawnsL);
+        while(r == oldR || !r) var r = Random(itemSpawnsL);
+
+        if(itemSpawns[r] == NH7A && !Random(2)) {
+            r = Random(itemSpawnsL);
+        }
+
         CreateContents(itemSpawns[r], spwn);
-   }
+
+        oldR = r;
+    }
 }
 func Script120(){
-     goto(2);
+    goto(2);
 }
