1a87a0d0c5059a93689caf08dea8ea0d453ba4d1
arthurmoeller <16727664+arthurmoeller@users.noreply.github.com>
2018-12-29 00:22:40 +0100

    asteriod meele - map overhaul

--- EkeSpace.c4f/EkeS_AsteroidMeele.c4s/Script.c
+++ EkeSpace.c4f/EkeS_AsteroidMeele.c4s/Script.c
@@ -1,25 +1,56 @@
- /*-- Asteriod Meele --*/
+/*-- Asteriod Meele --*/
 
 #strict 2
 
-func Initialize() {
-  // Sternenhimmel
-  SetSkyParallax(0, 100,70, 1,0);
-  CreateObject(CF7A);  
-  return(ScriptGo(1));
+static itemSpawns;
+static itemSpawnsL;
+static itemSpawnsR;
+
+func Initialize() { 
+    CreateItemSpawns();
+    return(ScriptGo(1));
 }
+
 func Script1()
 {
-  SetMaxPlayer();
-  return(1);
+    SetMaxPlayer();
+    return(1);
 }
 
 func Script2(){
+
+    // create items in spawn points
     for(var spwn in FindObjects(Find_ID(IS7A))) {
-    var Contents = [CA7A, OB7A, HG7A, GB7A, NH7A, GS7A, SG7A];
-    CreateContents(Contents[Random(GetLength(Contents))], spwn);
-   }
+
+        while(r == itemSpawnsR || !r) var r = Random(itemSpawnsL);
+
+        if(itemSpawns[r] == NH7A || itemSpawns[r] == SG7A || itemSpawns[r] == RL7A || itemSpawns[r] == RB7A) {
+            if(!Random(2)) {
+                r = Random(itemSpawnsL);
+            }
+        }
+
+        CreateContents(itemSpawns[r], spwn);
+
+        itemSpawnsR = r;
+    }
 }
 func Script120(){
-     goto(2);
+    goto(2);
+}
+
+/*------------------------------------*\
+    Item Spawns
+\*------------------------------------*/
+func CreateItemSpawns() {
+
+    // declare spawning items
+    itemSpawns = [CA7A, OB7A, GB7A, NH7A, SG7A, GS7A, RL7A, RB7A];
+    itemSpawnsL = GetLength(itemSpawns);
+
+    // spawn points
+    CreateObject(IS7A,1377,1008);
+    CreateObject(IS7A,952,538);
+    CreateObject(IS7A,773,759);
+    CreateObject(IS7A,1162,769);
 }
